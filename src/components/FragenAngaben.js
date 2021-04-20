2008322df39fd37ec0eeef3151e10fcce869d446
Elahesmailpour <eli_es87@yahoo.com>
2021-04-20 18:20:42 +0200

    test

--- src/components/FragenAngaben.js
+++ src/components/FragenAngaben.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 
 const FragenAngaben = ({ propsFrage, propsFragenLänge, propsFragenIndex }) => {
@@ -31,15 +32,17 @@ const FragenAngaben = ({ propsFrage, propsFragenLänge, propsFragenIndex }) => {
                 {propsFrage.answer.map((answerItem, answerIndex) =>
                
                     <li>
-                        <button onClick={() => AnwserButton(answerItem)}>{answerItem}</button>
+                        <button  onClick={() => AnwserButton(answerItem)}>{answerItem}</button>
                         {console.log("answerItem=",answerItem)}
                         {console.log("answerIndex=",answerIndex)}
                         {console.log("optionSelected=",showAnswer.optionSelected)}
-                        {showAnswer.show && propsFrage.correct === answerIndex && showAnswer.optionSelected === answerItem && <span>{"Richtige Antwort"}</span>}
-                        {showAnswer.show && showAnswer.optionSelected === answerItem && propsFrage.correct !== answerIndex && <span>{"Falsche Antwort"}</span>}
+                        {showAnswer.show && propsFrage.correct === answerIndex && showAnswer.optionSelected === answerItem && <span className="text-success">{"Richtige Antwort"}</span>}
+                        {showAnswer.show && showAnswer.optionSelected === answerItem && propsFrage.correct !== answerIndex && <span className="text-danger">{"Falsche Antwort"}</span>}
                     </li>
                 )}
             </ul>
+            
+       
         </div>
     )
 }
